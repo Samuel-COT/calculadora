d01d76420ee42902dfb64fca232578a07b031c4d
Samuel-COT <samuelcostasousa1000@gmail.com>
2024-09-23 15:41:11 -0300

    altercao de string

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,10 @@ function App() {
   const [tela, setTela] = useState('')
 
 
-  const valores = [1,2,3,'+',
-                   4,5, 6,'-',
-                   7,8, 9,'*',
-                   0,'.', '/']
+  const valores = ['1','2','3','+',
+                   '4','5', '6','-',
+                   '7','8', '9','*',
+                   '0','.', '/']
 
   
   return <>
